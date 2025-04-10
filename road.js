f3f103e28d26b1d4a294e17806346a305cb8dd2c
QuanN <jeanquan98@gmail.com>
2025-04-09 17:56:51 -0700

    refactor road

--- road.js
+++ road.js
@@ -10,6 +10,15 @@ class Road {
         const infinity = 1000000;
         this.top = -infinity;
         this.bottom = infinity;
+
+        const topLeft = { x:this.left, y: this.top };
+        const topRight = { x:this.right, y: this.top };
+        const bottomLeft = { x:this.left, y: this.bottom };
+        const bottomRight = { x:this.right, y: this.bottom };
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ];
     }
 
     draw(ctx) {
@@ -17,7 +26,7 @@ class Road {
         ctx.strokeStyle = 'white';
 
         // Lanes setup via linear interpolation
-        for (let i = 0; i <= this.laneCount; i++) {
+        for (let i = 1; i <= this.laneCount-1; i++) {
             const x = lerp(
                 this.left,
                 this.right,
@@ -25,8 +34,7 @@ class Road {
             );
 
             // Create dash lanes
-            if (i > 0 && i < this.laneCount) ctx.setLineDash([20,20]);
-            else ctx.setLineDash([]);
+            ctx.setLineDash([20,20]);
 
             // Draw lanes
             ctx.beginPath();
@@ -34,5 +42,17 @@ class Road {
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
         }
+        ctx.setLineDash([]);
+        this.borders.forEach(border => {
+            ctx.beginPath();
+            ctx.moveTo(border[0].x, border[0].y);
+            ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke();
+        })
+    }
+
+    getLaneCenter(laneIndex) {
+        const laneWidth = this.width / this.laneCount;
+        return this.left + laneWidth/2 + Math.min(laneIndex, this.laneCount-1) * laneWidth;
     }
 }
